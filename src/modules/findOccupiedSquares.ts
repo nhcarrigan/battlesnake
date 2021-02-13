7452a971fa624b1dd9f9b77ec4574a6d40dc6465
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-02-13 12:35:06 -0800

    fix: extra check for optional properties

--- src/modules/findOccupiedSquares.ts
+++ src/modules/findOccupiedSquares.ts
@@ -7,6 +7,9 @@ import { RequestBodyInt } from "../interfaces/RequestBodyInt";
  * @param {RequestBodyInt} data the API data received from the game client
  */
 export const findOccupiedSquares = (data: RequestBodyInt): CoordinateInt[] => {
+  if (!data.you || !data.board) {
+    return [];
+  }
   const list: CoordinateInt[] = [];
   list.push(...data.you.body);
   data.board.snakes.forEach((snake) => {

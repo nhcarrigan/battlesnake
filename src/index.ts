b3153aff41a522ccc06c776093bbe84045450c53
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-07-08 09:25:56 -0700

    feat: add https

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import { handleEnd } from "./controllers/handleEnd";
 import * as Sentry from "@sentry/node";
 import { RewriteFrames } from "@sentry/integrations";
 import { logHandler } from "./utils/logHandler";
+import http from "http";
+import https from "https";
+import { readFile } from "fs/promises";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -18,16 +21,46 @@ Sentry.init({
   ],
 });
 
-const PORT = process.env.PORT || 4000;
-
 export const app = express();
-app.use(bodyParser.json());
 
-app.get("/", handleIndex);
-app.post("/start", handleStart);
-app.post("/move", handleMove);
-app.post("/end", handleEnd);
+(async () => {
+  app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  logHandler.log("http", `BattleSnake Server listening on port ${PORT}`);
-});
+  app.get("/", handleIndex);
+  app.post("/start", handleStart);
+  app.post("/move", handleMove);
+  app.post("/end", handleEnd);
+
+  const httpServer = http.createServer(app);
+
+  httpServer.listen(1080, () => {
+    logHandler.log("http", "http server listening on port 1080");
+  });
+
+  if (process.env.NODE_ENV === "production") {
+    const privateKey = await readFile(
+      "/etc/letsencrypt/live/battlesnake.nhcarrigan.com/privkey.pem",
+      "utf8"
+    );
+    const certificate = await readFile(
+      "/etc/letsencrypt/live/battlesnake.nhcarrigan.com/cert.pem",
+      "utf8"
+    );
+    const ca = await readFile(
+      "/etc/letsencrypt/live/battlesnake.nhcarrigan.com/chain.pem",
+      "utf8"
+    );
+
+    const credentials = {
+      key: privateKey,
+      cert: certificate,
+      ca: ca,
+    };
+
+    const httpsServer = https.createServer(credentials, app);
+
+    httpsServer.listen(1443, () => {
+      logHandler.log("http", "https server listening on port 1443");
+    });
+  }
+})();
